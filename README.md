a6fb3d429d38a8a1aec392d34321f5a14ba88604
tylerGoulding <tgouldin@andrew.cmu.edu>
2017-08-24 11:03:02 -0400

    added some code and updated readme a lil

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
-# LED Display Controller
+# OddLED
 
 
-LED Display is a command-line tool to generate output arrays for addressable leds built on non-standard layout (e.g. custom PCB layouts or just arranged strips)
+OddLed is a command-line tool to generate output arrays for addressable leds for non-standard LED layout (e.g. custom PCB layouts or just arranged strips)
+
+## Features
+- Written in Python
+- Creates bitmaps using Photoshop files of LED layouts for unconventional led structures.
+- Supports multiple LED data lines (as seperate layers in the photoshop file)
+- Works with either single files as well as folders to generate full effects at a single time
+
+## Installation
+OddLED requires Python2 and a few 3rd Party Libraries
+To install python on Mac:
+```
+brew install python
+```
+To install Python on Debian/Ubuntu:
+```
+sudo apt-get install python
+```
+
+PIP should be installed on Python 2 >=2.7.9 but if you dont have it go [here](https://pip.pypa.io/en/stable/installing/)
+
+Once you have Pip, run:
+```
+pip install Pillow
+```
+```
+pip install PSD_tools
+```
 
 # How to:
 
--- led_controller.py
+++ led_controller.py
@@ -5,21 +5,21 @@ import sys, os
 import argparse
 
 
-
 class _ledArray():
     """Base Class for a single LED data stream."""
     def __init__(self,layer,color_mode='BW'):
+        self.color_mode = color_mode
         self.psd_layer = layer;
         self.name = layer.name;
         self.numLEDS = len(layer.layers);
         self.outputCount = [0 for i in xrange(self.numLEDS)];
         #self.outputFile = outputFile
         if color_mode == 'RGB':
-            self.outputArray =[ [-1,-1,-1] for i in xrange(self.numLEDS)]
+            self.outputArray =[ [0,0,0] for i in xrange(self.numLEDS)]
         else:
-            self.outputArray = [-1 for i in xrange(self.numLEDS)]
-    def generateOutput(self):
-        outputString = "const uint8_t " + self.name + "_" + "[] =  {"
+            self.outputArray = [0 for i in xrange(self.numLEDS)]
+    def generateOutput(self,effect):
+        outputString = "const uint8_t " + self.name + "_" + effect+"[] =  {"
         outputArray = [outputString]
         for byte in self.outputArray:
             outputArray.append(str(byte));
@@ -29,9 +29,16 @@ class _ledArray():
         outputString = ''.join([a for a in outputArray])
         print outputString
     def updateAverage(self,index,value):
-        count = self.outputCount[index] = self.outputCount[index] + 1;
-        oldMean = self.outputArray[index]
-        self.outputArray[index] = oldMean + (value-oldMean)/count
+        if (self.color_mode == "RGB"):
+            count = self.outputCount[index] = self.outputCount[index] + 1;
+            oldMean = self.outputArray[index]
+            for i in xrange(3):
+                self.outputArray[index][i] = oldMean[i] + (value[i]-oldMean[i])/count
+        else:
+
+            count = self.outputCount[index] = self.outputCount[index] + 1;
+            oldMean = self.outputArray[index]
+            self.outputArray[index] = oldMean + (value-oldMean)/count            
 
 verbose = False;
 
@@ -46,67 +53,70 @@ def parser_init():
     parser.add_argument("-m", "--mask",nargs=1,required=True,  help="Photoshop file of the LED positions.")
     parser.add_argument("-i", "--input",nargs=1,required=True, help="Photo (or Folder) of the input to overlay on to the LEDS. This should be the same pixel size as the mask!")
     parser.add_argument("-o", "--output",nargs=1,  type=argparse.FileType('w'), default=sys.stdout, help="Photoshop file of the LED positions.")
-    parser.add_argument("-c", "--color_mode",nargs=1, default="RGB", help="Color mode of input image (LA,RGB,RGBW,1) ")    
-    parser.add_argument("-v", "--verbose",nargs=1, action='store_true', help="Increase verbosity of ouput")    
+    parser.add_argument("-c", "--color_mode",nargs="?", default="RGB", help="Color mode of input image: (L,RGB,1)")    
+    parser.add_argument("-v", "--verbose", action='store_true', help="Increase verbosity of ouput")    
+    parser.add_argument("--labelled", action='store_true', help="Only use this if you have individually labelled all the layers of the leds in your image, otherwise, it is assumed that the LED are placed in order")    
 
     return parser;
 def main():
     #build parser options
     parser = parser_init();
+
     args = parser.parse_args()
     global verbose
     verbose = args.verbose;
     mask = args.mask[0]
-    color_mode = args.color_mode[0]
+    color_mode = args.color_mode
+    vprint("Input File: " + args.input[0])
+
     #open image and convert to given color mode
-    if os.path.isfile(args.input[0]):
+    if (os.path.isfile(args.input[0])):
         vprint("Single Image to process")
-        overlay = Image.open(args.input[0]).convert(color_mode)
+        files = [args.input[0]]
     elif os.path.isdir(args.input[0]):
         vprint("Using folder: " + args.input[0])
-        print "not implemented yet sorry"
-
-
-    #open photoshop image and convert to PIL.
+        files = os.listdir(args.input[0])
+    #open photoshop image
     maskPSD = PSDImage.load(mask)
     maskPIL = maskPSD.as_PIL();
-    maskPIL = maskPIL.convert("1");
-    maskMeta = maskPSD.header
-    # check if the dimesions of the mask and the overlay are the same
-    if ((maskMeta.width != overlay.width) and (maskMeta.height != overlay.height)):
-        raise("incorrect Size");
-    
-    print maskPSD.layers
+    maskPIL = maskPIL.convert("1"); #this convert mask to a bitmap
+    maskMeta = maskPSD.header           
+    for file in files:
+        if (color_mode == "1"):
+            overlay = Image.open(args.input[0]).convert(1)
+        else:
+            overlay = Image.open(file).convert(color_mode)
+        # check if the dimesions of the mask and the overlay are the same
+        if ((maskMeta.width != overlay.width) and (maskMeta.height != overlay.height)):
+            raise("Incorrect Size - Please apply a effect input file that is the same size as the LED Mask")
 
-
-    maskLayers = [ _ledArray(layer) for layer in maskPSD.layers ]
-    if (1):
-        print "Found " + str(len(maskLayers)) + " LED data lines"
-        print "These are the following layers:"
+        maskLayers = [ _ledArray(layer,color_mode) for layer in maskPSD.layers ]
+        vprint("Found " + str(len(maskLayers)) + " LED data lines")
+        vprint("These are the following layers:")
         for layer in maskLayers:
-            print "   " + layer.name
+            vprint("   " + layer.name)
 
-    for ledArray in maskLayers:
-        numLEDS = ledArray.numLEDS;
-        for i,led in enumerate(ledArray.psd_layer.layers):
-            # since layers loaded most to least recent, first led is the last in the led stream
-            index = numLEDS - i-1;
-            print index
-            x1,y1,x2,y2 = led.bbox;
-            # print "(",x1,y1,x2,y2,")"
-            # loop through bounding box and look for average value
-            # bbox doesnt return a tranformed bbox so we need to check if
-            # the pixel in the box is also part of the "led"
-            for x in xrange(x1,x2):
-                for y in xrange(y1,y2):
-                    pixel = maskPIL.getpixel((x,y))
-                    if (pixel == 0):
-                        # we have found a pixel, calculate average value
-                        intensity = overlay.getpixel((x,y))
-                        # print intensity[0]
-                        ledArray.updateAverage(index,intensity[0]);
-        ledArray.generateOutput()
-    # for 
+        # this is the meat of the function
+        for ledArray in maskLayers:
+            numLEDS = ledArray.numLEDS;
+            for i,led in enumerate(ledArray.psd_layer.layers):
+                # since layers loaded most to least recent, first led is the last in the led stream
+                index = numLEDS - i-1;
+                x1,y1,x2,y2 = led.bbox;
+                # print "(",x1,y1,x2,y2,")"
+                # loop through bounding box and look for average value
+                # bbox doesnt return a tranformed bbox so we need to check if
+                # the pixel in the box is also part of the "led"
+                for x in xrange(x1,x2):
+                    for y in xrange(y1,y2):
+                        pixel = maskPIL.getpixel((x,y))
+                        if (pixel == 0):
+                            # we have found a pixel, calculate average value
+                            intensity = overlay.getpixel((x,y))
+                            ledArray.updateAverage(index,intensity);
+            name = os.path.splitext(file)[0];
+            
+            ledArray.generateOutput(name)
 
 
 if __name__ == '__main__':
